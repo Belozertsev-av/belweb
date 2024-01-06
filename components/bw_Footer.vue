fdc591a8e5be298adaddfef5bf8dfff3c6d33a0e
belozertsev_aw <andrew.vl.belozertsev@gmail.com>
2024-01-06 12:29:10 +0300

    Откалиброваны основные цвета

--- components/bw_Footer.vue
+++ components/bw_Footer.vue
@@ -13,5 +13,20 @@
 </script>
 
 <style lang="scss" scoped>
+html.dark-mode{
+    .footer{
+        background-color: $backgroundColorDark;
+    }
+}
+.footer {
+    box-shadow: 0 0 4px $shadowColor;
+    background-color: $backgroundColor;
+    &__body {
+        margin: 0 auto;
+        width: 100%;
+        @include adaptive-value("padding", 30, 15, 0);
+        text-align: center;
+    }
+}
 
 </style>
--- components/main/FirstComponent.vue
+++ components/main/FirstComponent.vue
@@ -34,7 +34,7 @@
         </div>
     </section>
 </template>
-
+ смм
 <script setup lang="ts">
 
 </script>
