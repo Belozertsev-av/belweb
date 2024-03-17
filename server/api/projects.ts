fe47f089f8df4afa1dc11556055c3d627a0354b7
Belozertsev-av <andrew.vl.belozertsev@gmail.com>
2024-03-17 14:24:32 +0300

    hotfix

--- server/api/projects.ts
+++ server/api/projects.ts
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/core"
 
 const octokit = new Octokit({
-  auth: "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"
+  auth: process.env.API_KEY
 })
 
 export default defineEventHandler(async (event) => {
